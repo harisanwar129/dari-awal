ed79625b5971635a90d5ce9511600e44e222c750
harisanwar129 <harisanwar129@gmail.com>
2020-06-08 14:59:56 +0700

    3

---
+++ resources/views/grid.blade.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+     <!--<div class="container">
+      <h1>Grid System</h1>
+
+      <div class="row">
+          <div class="col-xs-12 col-md-8">.col-xs-12 .col-md-8</div>
+          <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+      </div>
+       <div class="row">
+          <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+         <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+         <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+     </div>
+      <div class="row">
+          <div class="col-xs-6">.col-xs-6</div>
+          <div class="col-xs-6">.col-xs-6</div>
+      </div>
+  </div>-->
+   <!--<form class="form-inline">
+     <div class="form-group">
+		<label class="sr-only" for="exampleInputEmail3">Email address</label>
+       <input type="email" class="form-control" id="exampleInputEmail3"
+placeholder="Email">
+     </div>
+     <div class="form-group">
+		<label class="sr-only" for="exampleInputPassword3">Password</label>
+		<input type="password" class="form-control" id="exampleInputPassword3" placeholder="Password">
+     </div>
+   <div class="checkbox">
+      <label>
+        <input type="checkbox"> Remember me
+      </label>
+    </div>
+    <button type="submit" class="btn btn-default">Sign in</button>
+  </form>-->
+
+<!--<form class="form-horizontal">
+    <div class="form-group">
+		<label for="inputEmail" class="col-sm-2 control- label">Email</label>
+       <div class="col-sm-10">
+		<input type="email" class="form-control" id="inputEmail" placeholder="Email">
+       </div>
+     </div>
+
+     <div class="form-group">
+      <label for="inputPassword" class="col-sm-2 control-
+label">Password</label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="inputPassword"
+placeholder="Password"> 
+      </div>
+    </div>
+
+    <div class="form-group">
+		<label for="inputUsername" class="col-sm-2 control- label">Username</label>
+      <div class="col-sm-10">
+             <div class="input-group">
+               <div class="input-group-addon"></div>
+               <input type="username" class="form-control"
+id="inputUsername" placeholder="Username">
+             </div>
+      </div>
+    </div>
+
+
+    <div class="form-group">
+		<label for="inputAlamat" class="col-sm-2 control- label">Alamat</label>
+         <div class="col-sm-10">
+		<textarea class="form-control" rows="3" id="inputAlamat" placeholder="Alamat"></textarea>
+         </div>
+    </div>
+    
+    <div class="form-group">
+		<label for="optionRadios" class="col-sm-2 control- label">Jenis Kelamin</label>
+         <div class="col-sm-10">
+                <div class="radio">
+                       <label class="radio-inline">
+                              <input type="radio" name="optionRadios"
+id="inputJKelamin1" value="Pria" checked> Pria
+                        </label>
+                        <label class="radio-inline">
+		<input type="radio" name="optionRadios" id="inputJKelamin2" value="Wanita"> Wanita
+                       </label>
+                 </div>
+         </div>
+   </div>
+
+   
+    <div class="form-group">
+        <label for="inputKelas" class="col-sm-2 control-
+label">Kelas</label>
+         <div class="col-sm-10">
+                 <select class="form-control">
+                        <option>A</option>
+                        <option>B</option>
+                        <option>C</option>
+                 </select>
+         </div>
+   </div> 
+
+
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+       <div class="checkbox">
+          <label>
+           <input type="checkbox"> Remember me
+          </label>
+        </div>
+      </div>
+    </div>
+
+    <div class="form-group">
+     <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Sign in</button>
+      </div>
+    </div>
+  </form>-->
+<a class="btn btn-default" href="#" role="button">Link</a>
+   <button class="btn btn-primary" type="button">Button</button>
+   <input class="btn btn-success" type="button" value="Input">
+   <input class="btn btn-danger" type="submit" value="Submit">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');  
 });
+
+
+Route::get('boostrap','MainController@grid');
